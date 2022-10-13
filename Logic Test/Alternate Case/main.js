cbc8de3c7117871da5e976c14a11a70dac02eec1
Faishql <68933048+Faishql@users.noreply.github.com>
2022-10-13 16:02:52 +0700

    update

--- Logic Test/Alternate Case/main.js
+++ Logic Test/Alternate Case/main.js
@@ -1,6 +1,7 @@
 var kalimat = prompt("Masukkan kalimat!");
 
 ubah(kalimat);
+document.getElementById("output").innerHTML = "yourTextHere";
 // console.log(splitedNumber);
 
 function ubah(a) {
--- Mini App/tailwind.config.js
+++ Mini App/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./app/index.html"],
+    theme: {
+        extend: {},
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            "sec-green": "rgb(19, 164, 88)",
+            "bg-green": "#71CA9B",
+            "pri-orange": "rgb(249, 129, 58)",
+            "text-color": "#263238",
+            "bg-white": "#FFFFFF",
+        },
+    },
+    plugins: [],
+};
