40eee88b07e8b65a52b42f59e4c6f5aec3bdbe27
Faishql <68933048+Faishql@users.noreply.github.com>
2022-10-13 16:25:02 +0700

    u

--- Logic Test/Alternate Case/main.js
+++ Logic Test/Alternate Case/main.js
@@ -1,7 +1,6 @@
 var kalimat = prompt("Masukkan kalimat!");
 
 ubah(kalimat);
-document.getElementById("output").innerHTML = "yourTextHere";
 // console.log(splitedNumber);
 
 function ubah(a) {
--- Logic Test/Nearest Fibonacci/main.js
+++ Logic Test/Nearest Fibonacci/main.js
@@ -1,9 +1,7 @@
 var i;
-var fib = [0, 1]; // Initialize array!
+var f = [15, 1, 3];
 
 for (i = 2; i <= 10; i++) {
-    // Next fibonacci number = previous + one before previous
-    // Translated to JavaScript:
-    fib[i] = fib[i - 2] + fib[i - 1];
-    console.log(fib[i]);
+    f[i] = f[i - 2] + f[i - 1];
+    console.log(f[i]);
 }
